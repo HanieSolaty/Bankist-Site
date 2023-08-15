09256977c195be0d521e4713e863419dd2440225
HanieSolaty <88339589+HanieSolaty@users.noreply.github.com>
2023-08-15 13:42:04 +0330

    Adding smooth scroll option to navlinks in navbar

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+//navbar
+const navLinks = document.querySelector('.nav__links');
+
 //Modal window functions
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -35,6 +38,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//Smooth scroll to section 1
+
 btnScrollTo.addEventListener('click', () => {
   /*TODO:1.getBoundingClientRect can be called on any element and it will return width hieght and 
   coordinates of element including x y and space from top left right bottom just CONSIDER 
@@ -57,3 +62,31 @@ btnScrollTo.addEventListener('click', () => {
   //element.scrollIntoView() only takes behavior and we DON'T need to calculate scroll and el top and left
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+//nav links smooth scrolling to thier related section
+function navLinksScroll(e) {
+  e.preventDefault();
+  /*since we have attached the eventListener to parent element, Now we need a Mathching strategy other wise even when we
+   click on .nav__links space where there are no link this will get executed but we need this for only real links*/
+  //e.target.classList.length === 1 this condition is to exclude .nav__link--btn from other .nav__link elements
+  //Matching Strategy
+  if (
+    e.target.classList.contains('nav__link') &&
+    e.target.classList.length === 1
+  ) {
+    /*the href is same as id of the section we want to scroll to so we can get that from .nav__link 
+    and scroll to section with same id, however we should use getArribute method otherwise we get 
+    the complete absolute href, something like site.com/sectionid or 127.0.0.1:8080/sectionid
+    but no section has that id, we ONLY need the relative href which is section id*/
+    const matchingSection = document.querySelector(
+      e.target.getAttribute('href')
+    );
+    matchingSection.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+/*adding Eventlistener to parent element of each of the .navlink elements which is .navlinks
+if an click event happen at any of .navlink elements, then during bubbling phase it will also happen at thier parent,
+and we use that instead of attaching the event to all the link, this will prevent us from OVERWRITING our function for
+every .nav__link, this way our function is only written once for parent element*/
+navLinks.addEventListener('click', navLinksScroll);
